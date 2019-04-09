5620cdc0d0922e4ab0bde2d0ad930d3d9f47d896
rdaripgr <49423052+rdaripgr@users.noreply.github.com>
2019-04-09 11:02:15 +0700

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,9 @@
+<script type= "text/javascript" src="path-to-javascript-file.js"></script>
+/* effects */
+/* Demo purposes only */
+    $(".hover").mouseleave(
+        function () {
+        $(this).removeClass("hover");
+        }
+    );
+/* end effects */
